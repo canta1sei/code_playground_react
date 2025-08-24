f3a396352fa2519bb2365df56334cd91b5fdc278
Taisei Kyan <taisei.kyan.job@gmail.com>
2025-08-24 11:12:32 +0900

    検証用のコードを追加と文言の微修正

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -215,7 +215,7 @@ function App() {
               <button 
                 onClick={handleDownloadImage} 
                 className="sparkle-button w-full max-w-xs bg-gradient-to-r from-cyan-400 to-blue-500 text-white font-bold py-3 px-8 rounded-full shadow-lg hover:shadow-xl transition-all duration-300">
-                  画像を保存してXに共有
+                  画像を保存
               </button>
               <button 
                 onClick={handleShare} 
