9b97477e5f9f749e16c33590bba633869d6abbfc
Taisei Kyan <taisei.kyan.job@gmail.com>
2025-10-26 14:32:20 +0900

    ドメインの登録

--- infra/lib/infra-stack.ts
+++ infra/lib/infra-stack.ts
@@ -6,20 +6,23 @@ import * as apigw from 'aws-cdk-lib/aws-apigatewayv2';
 import { HttpLambdaIntegration } from 'aws-cdk-lib/aws-apigatewayv2-integrations';
 import * as path from 'path';
 import * as s3 from 'aws-cdk-lib/aws-s3';
-import * as s3deploy from 'aws-cdk-lib/aws-s3-deployment';
 import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
 import * as origins from 'aws-cdk-lib/aws-cloudfront-origins';
+import * as route53 from 'aws-cdk-lib/aws-route53';
+import * as targets from 'aws-cdk-lib/aws-route53-targets';
+import * as acm from 'aws-cdk-lib/aws-certificatemanager';
 
 // スタックに渡すプロパティの型を定義
 export interface InfraStackProps extends cdk.StackProps {
   envName: 'dev' | 'prod';
+  certificate?: acm.ICertificate; // <- 追加: 証明書を外部から受け取る
 }
 
 export class InfraStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props: InfraStackProps) {
     super(scope, id, props);
 
-    const { envName } = props;
+    const { envName, certificate } = props; // <- certificate を受け取る
     const suffix = `-${envName}`;
 
     // 環境に応じた削除ポリシーを決定 (prodではリソースを保持)
@@ -73,19 +76,59 @@ export class InfraStack extends cdk.Stack {
       autoDeleteObjects: envName === 'dev',
     });
 
-    // CloudFront ディストリビューション
-        const distribution = new cloudfront.Distribution(this, `CloudFrontDistribution${suffix}`, {
-      defaultBehavior: {
-        origin: origins.S3BucketOrigin.withOriginAccessControl(frontendBucket),
-        viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
-        responseHeadersPolicy: cloudfront.ResponseHeadersPolicy.SECURITY_HEADERS,
-      },
-      defaultRootObject: 'index.html',
-      errorResponses: [
-        { httpStatus: 403, responseHttpStatus: 200, responsePagePath: '/index.html' },
-        { httpStatus: 404, responseHttpStatus: 200, responsePagePath: '/index.html' },
-      ],
-    });
+    // --- ここからドメイン関連の設定 ---
+    let distribution;
+
+    if (envName === 'prod') {
+      const domainName = 'tdf-arena.com';
+      const hostedZone = route53.PublicHostedZone.fromHostedZoneAttributes(this, 'HostedZone', {
+        zoneName: domainName,
+        hostedZoneId: 'Z07566193RAUOSUIHU9W5',
+      });
+
+      // ▼▼▼ 変更点 ▼▼▼
+      // 証明書作成ロジックを削除し、propsから受け取った証明書を利用する
+      if (!certificate) {
+        throw new Error('Certificate is required for prod environment');
+      }
+
+      // CloudFront ディストリビューション (prod)
+      distribution = new cloudfront.Distribution(this, `CloudFrontDistribution${suffix}`, {
+        defaultBehavior: {
+          origin: origins.S3BucketOrigin.withOriginAccessControl(frontendBucket),
+          viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+          responseHeadersPolicy: cloudfront.ResponseHeadersPolicy.SECURITY_HEADERS,
+        },
+        domainNames: [domainName],
+        certificate: certificate, // <- ここで受け取った証明書を利用
+        defaultRootObject: 'index.html',
+        errorResponses: [
+          { httpStatus: 403, responseHttpStatus: 200, responsePagePath: '/index.html' },
+          { httpStatus: 404, responseHttpStatus: 200, responsePagePath: '/index.html' },
+        ],
+      });
+
+      // Route 53にAレコードを登録
+      new route53.ARecord(this, 'ARecord', {
+        zone: hostedZone,
+        target: route53.RecordTarget.fromAlias(new targets.CloudFrontTarget(distribution)),
+      });
+    } else {
+      // CloudFront ディストリビューション (dev)
+      distribution = new cloudfront.Distribution(this, `CloudFrontDistribution${suffix}`, {
+        defaultBehavior: {
+          origin: origins.S3BucketOrigin.withOriginAccessControl(frontendBucket),
+          viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+          responseHeadersPolicy: cloudfront.ResponseHeadersPolicy.SECURITY_HEADERS,
+        },
+        defaultRootObject: 'index.html',
+        errorResponses: [
+          { httpStatus: 403, responseHttpStatus: 200, responsePagePath: '/index.html' },
+          { httpStatus: 404, responseHttpStatus: 200, responsePagePath: '/index.html' },
+        ],
+      });
+    }
+    // --- ここまでドメイン関連の設定 ---
 
 
     // API Gateway
@@ -114,22 +157,22 @@ export class InfraStack extends cdk.Stack {
     // アウトプット
     new cdk.CfnOutput(this, `ApiEndpoint${suffix}`, {
       value: httpApi.url!,
-      exportName: `ApiEndpoint${suffix}`,
+      exportName: `ApiEndpoint${suffix}`
     });
 
     new cdk.CfnOutput(this, `FrontendBucketName${suffix}`, {
       value: frontendBucket.bucketName,
-      exportName: `FrontendBucketName${suffix}`,
+      exportName: `FrontendBucketName${suffix}`
     });
 
     new cdk.CfnOutput(this, `CloudFrontDistributionId${suffix}`, {
       value: distribution.distributionId,
-      exportName: `CloudFrontDistributionId${suffix}`,
+      exportName: `CloudFrontDistributionId${suffix}`
     });
 
     new cdk.CfnOutput(this, `CloudFrontUrl${suffix}`, {
       value: distribution.distributionDomainName,
-      exportName: `CloudFrontUrl${suffix}`,
+      exportName: `CloudFrontUrl${suffix}`
     });
   }
 }
